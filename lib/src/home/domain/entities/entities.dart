b7473c149a6ee28842968f44e38ef47f6ea8f0ed
Levi Vieira <levivcruz@gmail.com>
2025-08-29 15:08:03 -0300

    feat: domain created

---
+++ lib/src/home/domain/entities/entities.dart
@@ -0,0 +1,3 @@
+export 'current_exchange_rate_entity.dart';
+export 'exchange_rate_data_entity.dart';
+export 'exchange_rate_history_entity.dart';
---
+++ lib/src/home/domain/usecases/get_current_exchange_rate_usecase.dart
@@ -0,0 +1,47 @@
+import '../../../../core/shared/errors/either.dart';
+import '../../../../core/shared/errors/failures.dart';
+import '../../../../core/shared/validators/currency_validator.dart';
+import '../entities/current_exchange_rate_entity.dart';
+import '../../data/repositories/repositories.dart';
+
+abstract interface class IGetCurrentExchangeRateUsecase {
+  Future<Either<Failure, CurrentExchangeRateEntity>> call({
+    required String fromSymbol,
+    required String toSymbol,
+  });
+}
+
+class GetCurrentExchangeRateUsecase implements IGetCurrentExchangeRateUsecase {
+  final IExchangeRateRepository _repository;
+
+  const GetCurrentExchangeRateUsecase(this._repository);
+
+  @override
+  Future<Either<Failure, CurrentExchangeRateEntity>> call({
+    required String fromSymbol,
+    required String toSymbol,
+  }) async {
+    try {
+      if (!CurrencyValidator.isValid(fromSymbol) ||
+          !CurrencyValidator.isValid(toSymbol)) {
+        return const Left(
+          InvalidInputFailure(message: 'Invalid currency code provided'),
+        );
+      }
+
+      final result = await _repository.getCurrentExchangeRate(
+        fromSymbol: fromSymbol.toUpperCase(),
+        toSymbol: toSymbol.toUpperCase(),
+      );
+
+      return result;
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          message:
+              'Unexpected error while getting exchange rate: ${e.toString()}',
+        ),
+      );
+    }
+  }
+}
